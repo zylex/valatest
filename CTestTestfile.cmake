e6acce67d1dd69287f80f60690c9ce0c4680449e
Zylex <zylex.stk@gmail.com>
2013-04-22 23:03:58 +0200

    Cleaned up

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zylex/Dropbox/workspace/Vala/EntropiaLandAreaManager/tests/lib
-# Build directory: /home/zylex/Dropbox/workspace/Vala/EntropiaLandAreaManager/tests/lib
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
